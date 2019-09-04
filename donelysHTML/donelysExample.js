9b6f80494d0c567dc54f579abdc70f75e9336e47
Donelys Familia <donelysprivate@gmail.com>
2019-09-04 14:44:28 -0400

    adding more fonts and comments

--- donelysHTML/donelysExample.js
+++ donelysHTML/donelysExample.js
@@ -6,6 +6,7 @@ function getDate(){
 }
 
 
+/*** this functin will take a color parameter and will change the actual color of element ***/
 function getNewColor(newColor){
  var coloring = getElementById('seconForm');
  coloring.style.color = newColor;
