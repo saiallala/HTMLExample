2006030e100c986d7e2e1f860d71a8a227a35219
Donelys Familia <donelysprivate@gmail.com>
2019-09-04 14:14:09 -0400

    adding more fonts

---
+++ donelysHTML/donelysExample.js
@@ -0,0 +1,13 @@
+
+/*** this is a function that returns the actual date */
+
+function getDate(){
+  var date = document.getElementById('dateArea').innerHTML = Date();
+}
+
+
+function getNewColor(newColor){
+ var coloring = getElementById('seconForm');
+ coloring.style.color = newColor;
+
+}
